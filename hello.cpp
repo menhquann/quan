6328f1cd12ccadb54c3c9450b5e5f0b7ce195f16
menhquann <102190284@sv1.dut.udn.vn>
2021-03-10 13:53:57 +0700

    4

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(){
-<<<<<<< HEAD
-    cout<<"hello world ."<<endl;
-=======
     cout<<"hello world"<<endl;
-    cout<<"i'm yours";
->>>>>>> test
+    cout<<"i'm tuesday";
 }
 
